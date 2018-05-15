6bf139d6f535d92f2882e12342aedeb8d2240f2f
maanlamp <32102935+maanlamp@users.noreply.github.com>
2018-05-15 11:56:50 +0200

    Corrected (un)maximise behaviour

--- assets/js/titlebar.js
+++ assets/js/titlebar.js
@@ -2,6 +2,8 @@ const electron = require("electron");
 const { remote } = electron;
 const { BrowserWindow } = remote;
 const display = electron.screen;
+let prevSize;
+let prevPos;
 
 const title = document.querySelector("h1#title");
 title.textContent = document.querySelector("title").textContent;
@@ -14,6 +16,7 @@ document.querySelector("#minimise").addEventListener("click", () => {
 	remoteWindow.minimize();
 });
 
+//Had to manually set size and pos bc unmaximize didn't work.
 document.querySelector("#maximise").addEventListener("click", () => {
 	const remoteWindow = remote.getCurrentWindow();
 	const appContainer = document.querySelector("#appContainer");
@@ -21,11 +24,17 @@ document.querySelector("#maximise").addEventListener("click", () => {
 	appContainer.classList.toggle("fullscreen");
 	html.classList.toggle("fullscreen");
 	if (appContainer.classList.contains("fullscreen")) {
+		prevSize = remoteWindow.getSize();
+		prevPos = remoteWindow.getPosition();
 		remoteWindow.maximize();
 	} else {
-		remoteWindow.setSize(1000, 640);
+		remoteWindow.setSize(prevSize[0] || 1000, prevSize[1] || 640);
+		if (prevPos) {
+			remoteWindow.setPosition(prevPos[0], prevPos[1]);
+		} else {
+			remoteWindow.center();
+		}
 	}
-	remoteWindow.center();
 });
 
 document.querySelector("#close").addEventListener("click", () => {
