2cf8c2afdbef0610a513d6b9fdda899eb51e2238
maanlamp <32102935+maanlamp@users.noreply.github.com>
2018-05-15 13:03:42 +0200

    Fixed usability problem
    Fixed a problem where pressing enter when focussed on a button would click it twice.

--- assets/js/clickWithEnter.js
+++ assets/js/clickWithEnter.js
@@ -1,4 +1,4 @@
 document.addEventListener("keyup", event => {
-	if (event.key !== "Enter" || document.activeElement === document.body) return;
+	if (event.key !== "Enter" || document.activeElement === document.body || document.activeElement.tagName === "BUTTON") return;
 	document.activeElement.click();
 });
