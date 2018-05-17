f39071d80b508e4ca28e998f1f4c48e01853c91a
maanlamp <32102935+maanlamp@users.noreply.github.com>
2018-05-17 11:35:34 +0200

    Correctly calulate when to round scrollbar
    Changed from an absolute minimal number to a minimal percentage after which to smooth the scrollbar bottom. Default is .75

--- assets/js/scrollbar.js
+++ assets/js/scrollbar.js
@@ -1,14 +1,14 @@
 const itemlist = document.querySelector("#itemList");
-const minimalDistance = 300;
+const minmialPercentage = .75;
 const html = document.querySelector("html");
 
 itemlist.addEventListener("scroll", event => {
 	if (html.classList.contains("fullscreen")) return;
 	const viewportBottom = itemlist.scrollTop + itemlist.clientHeight;
 	const windowBottom = itemlist.scrollHeight;
-	const distanceBetweenBottoms = windowBottom - viewportBottom;
-	if (distanceBetweenBottoms < minimalDistance) {
-		const factor = (minimalDistance - distanceBetweenBottoms) / minimalDistance * .6;
+	const scrollPercentage = viewportBottom / windowBottom;
+	if (scrollPercentage > minmialPercentage) {
+		const factor = (scrollPercentage - minmialPercentage) / (1 - minmialPercentage) * .6; //*.6 is needed bc 1rem somehow is different in js than in css
 		itemlist.style.setProperty("--scrollbarBorderRadius", `${factor}rem`);
 	}
 	
